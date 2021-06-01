d28a58a58221bd63dc6836699f4674536105b53a
caiyiheng <yiheng.cai@centurygame.com>
2021-06-01 19:37:31 +0800

    add bracket

--- luabinding.c
+++ luabinding.c
@@ -373,8 +373,9 @@ compat_luaL_setfuncs(lua_State *l, const luaL_Reg *reg, int nup)
 
     luaL_checkstack(l, nup, "too many upvalues");
     for (; reg->name != NULL; reg++) { /* fill the table with given functions */
-        for (i = 0; i < nup; i++)      /* copy upvalues to the top */
+        for (i = 0; i < nup; i++) {     /* copy upvalues to the top */
             lua_pushvalue(l, -nup);
+        }
         lua_pushcclosure(l, reg->func, nup); /* closure with those upvalues */
         lua_setfield(l, -(nup + 2), reg->name);
     }
